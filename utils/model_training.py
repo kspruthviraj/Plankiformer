17739d02c011aeda56ca3a91f51a18cb3a6130c8
Sreenath P Kyathanahally <pruthvi.iyengar@gmail.com>
2022-06-16 17:29:04 +0200

    First_version

--- utils/model_training.py
+++ utils/model_training.py
@@ -100,6 +100,7 @@ class import_and_train_model:
             test_accuracy = accuracy_score(test_outputs, test_targets)
 
             best_acc1 = max(acc1, best_acc1)
+            print('This is the name: {}'.format(name))
 
             if test_f1 > best_f1:
                 torch.save({'model_state_dict': self.model.state_dict(),
