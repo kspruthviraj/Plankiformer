76c848cbd3f1214e1ff52e5b2371e5570be1f896
Sreenath P Kyathanahally <pruthvi.iyengar@gmail.com>
2022-06-17 17:38:18 +0200

    To_check_duplicate_files

--- check_duplicates.py
+++ check_duplicates.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # if running in py3, change the shebang, drop the next import for readability (it does no harm in py3)
-from __future__ import print_function   # py2 compatibility
+from __future__ import print_function  # py2 compatibility
 from collections import defaultdict
 import hashlib
 import os
@@ -31,10 +31,10 @@ def get_hash(filename, first_chunk_only=False, hash=hashlib.sha1):
     return hashed
 
 
-def check_for_duplicates(paths, hash=hashlib.sha1):
+def check_for_duplicates(paths, outpath):
     hashes_by_size = defaultdict(list)  # dict of size_in_bytes: [full_path_to_file1, full_path_to_file2, ]
     hashes_on_1k = defaultdict(list)  # dict of (hash1k, size_in_bytes): [full_path_to_file1, full_path_to_file2, ]
-    hashes_full = {}   # dict of full_file_hash: full_path_to_file_string
+    hashes_full = {}  # dict of full_file_hash: full_path_to_file_string
 
     for path in paths:
         for dirpath, dirnames, filenames in os.walk(path):
@@ -51,11 +51,10 @@ def check_for_duplicates(paths, hash=hashlib.sha1):
                     # not accessible (permissions, etc) - pass on
                     continue
 
-
     # For all files with the same file size, get their hash on the 1st 1024 bytes only
     for size_in_bytes, files in hashes_by_size.items():
         if len(files) < 2:
-            continue    # this file size is unique, no need to spend CPU cycles on it
+            continue  # this file size is unique, no need to spend CPU cycles on it
 
         for filename in files:
             try:
@@ -71,7 +70,9 @@ def check_for_duplicates(paths, hash=hashlib.sha1):
     # For all files with the hash on the 1st 1024 bytes, get their hash on the full file - collisions will be duplicates
     for __, files_list in hashes_on_1k.items():
         if len(files_list) < 2:
-            continue    # this hash of fist 1k file bytes is unique, no need to spend cpy cycles on it
+            continue  # this hash of fist 1k file bytes is unique, no need to spend cpy cycles on it
+
+        f = open(outpath + 'duplicate_report_.txt', 'w')
 
         for filename in files_list:
             try:
@@ -79,11 +80,14 @@ def check_for_duplicates(paths, hash=hashlib.sha1):
                 duplicate = hashes_full.get(full_hash)
                 if duplicate:
                     print("Duplicate found: {} and {}".format(filename, duplicate))
+                    f.write("Duplicate found: {} and {}".format(filename, duplicate))
                 else:
                     hashes_full[full_hash] = filename
             except (OSError,):
                 # the file access might've changed till the exec point got here
                 continue
+
+        f.close()
 
 
 if __name__ == "__main__":
