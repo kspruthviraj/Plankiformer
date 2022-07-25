03a71e489a684bfab717304fbe97bdb60782d1c4
Sreenath P Kyathanahally <pruthvi.iyengar@gmail.com>
2022-07-25 19:15:18 +0200

    Debug predict_labeled.py

--- utils/for_plankton_test.py
+++ utils/for_plankton_test.py
@@ -98,7 +98,7 @@ class CreateDataForPlankton:
         data_train = 255 * data_train
         self.X_train = data_train.astype(np.uint8)
         # self.y_train = np.array([classes_int[y_train_max[i]] for i in range(len(y_train_max))], dtype=object)
-        y_train = np.concatenate([np.where(classes == uid) if np.where(classes == uid) else print(
+        y_train = np.concatenate([np.where(self.classes == uid) if np.where(self.classes == uid) else print(
             'The folder should match the trained classes') for uid in trY]).ravel()
 
         print('I am printing self.y_train : {}'.format(y_train))
