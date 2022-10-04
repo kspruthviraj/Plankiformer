492246f8a3fc2ea59c53f5d9a421cf919651d25a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 05:09:52 +0000

    Adding numeric_owner as keyword arguement

--- utils/for_dogs.py
+++ utils/for_dogs.py
@@ -298,7 +298,7 @@ class dogs(data.Dataset):
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
                 
-                    tar.extractall(path, members, numeric_owner) 
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
                     
                 
                 safe_extract(tar_file, self.root)
