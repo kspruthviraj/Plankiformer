81ae19107e8f7012270ce08f7f8952160413a7c5
Sreenath P Kyathanahally <pruthvi.iyengar@gmail.com>
2022-10-31 14:59:22 +0100

    Save test dataloader - to debug

--- utils/for_plankton_test.py
+++ utils/for_plankton_test.py
@@ -107,13 +107,13 @@ class CreateDataForPlankton:
         self.test_dataloader = DataLoader(test_dataset, 32, shuffle=False, num_workers=0,
                                           pin_memory=True)
 
-    def create_data_loaders_with_y(self, train_main):
+    def create_data_loaders_with_y(self, test_main):
         # self.checkpoint_path = test_main.params.model_path
 
         test_dataset = CreateDataset_with_y(X=self.X_train, y=self.y_train)
         self.test_dataloader = DataLoader(test_dataset, 32, shuffle=False, num_workers=0,
                                           pin_memory=True)
-        torch.save(self.test_dataloader, train_main.params.main_param_path + '/test_dataloader.pth')
+        torch.save(self.test_dataloader, test_main.params.main_param_path + '/test_dataloader.pth')
 
 
 class CreateDataset(Dataset):
