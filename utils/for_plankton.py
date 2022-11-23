71b0601e861e8238b357ada011c853626e3d4337
Sreenath P Kyathanahally <pruthvi.iyengar@gmail.com>
2022-11-23 16:10:44 +0100

    Debug model_training.py to correct the differences between ensemble and predict_labelled predictions

--- utils/for_plankton.py
+++ utils/for_plankton.py
@@ -229,7 +229,7 @@ class CreateDataForPlankton:
 
         if train_main.params.test_set == 'yes' and train_main.params.valid_set == 'yes':
             train_dataset = AugmentedDataset(X=self.X_train, y=self.y_train, aug_type=train_main.params.aug_type)
-            self.train_dataloader = DataLoader(train_dataset, train_main.params.batch_size, shuffle=True, num_workers=4,
+            self.train_dataloader = DataLoader(train_dataset, train_main.params.batch_size, shuffle=False, num_workers=4,
                                                pin_memory=True)
 
             test_dataset = CreateDataset(X=self.X_test, y=self.y_test)
@@ -241,7 +241,7 @@ class CreateDataForPlankton:
                                              pin_memory=True)
         elif train_main.params.test_set == 'no':
             train_dataset = AugmentedDataset(X=self.X_train, y=self.y_train, aug_type=train_main.params.aug_type)
-            self.train_dataloader = DataLoader(train_dataset, train_main.params.batch_size, shuffle=True, num_workers=4,
+            self.train_dataloader = DataLoader(train_dataset, train_main.params.batch_size, shuffle=False, num_workers=4,
                                                pin_memory=True)
         elif train_main.params.test_set == 'yes' and train_main.params.valid_set == 'no':
             train_dataset = AugmentedDataset(X=self.X_train, y=self.y_train, aug_type=train_main.params.aug_type)
@@ -303,7 +303,7 @@ class CreateDataForPlankton:
         Path(self.checkpoint_path).mkdir(parents=True, exist_ok=True)
 
         train_dataset = AugmentedDataset(X=self.X_train, y=self.y_train, aug_type=train_main.aug_type)
-        self.train_dataloader = DataLoader(train_dataset, train_main.params.batch_size, shuffle=True, num_workers=4,
+        self.train_dataloader = DataLoader(train_dataset, train_main.params.batch_size, shuffle=False, num_workers=4,
                                            pin_memory=True)
 
         test_dataset = CreateDataset(X=self.X_test, y=self.y_test)
