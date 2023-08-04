ee89437b986293c115fde10f2f2790f1813b8af5
Kyathanahally <kyathasr@siam-linux20.eawag.wroot.emp-eaw.ch>
2023-08-04 13:21:14 +0200

    added noise augmentations

--- utils/for_plankton.py
+++ utils/for_plankton.py
@@ -9,6 +9,8 @@ import pandas as pd
 import torch
 import torchvision.transforms as T
 from torch.utils.data import DataLoader, Dataset
+import cv2
+from scipy.ndimage import gaussian_filter
 
 np.random.seed(0)
 torch.manual_seed(0)
@@ -321,143 +323,64 @@ class CreateDataForPlankton:
                                          pin_memory=True)
 
 
-class AddPoissonNoise(object):
-    def __init__(self, scale_factor_range=(1., 10.)):
-        self.scale_factor_range = scale_factor_range
-
-    def __call__(self, tensor):
-        scale_factor = torch.FloatTensor(1).uniform_(*self.scale_factor_range).item()
-        return (torch.poisson(tensor * scale_factor) / scale_factor).float()
-
-    def __repr__(self):
-        return self.__class__.__name__ + '(scale_factor_range={0})'.format(self.scale_factor_range)
-
-
-class AddSaltPepperNoise(object):
-    def __init__(self, salt_prob_range=(0.01, 0.05), pepper_prob_range=(0.01, 0.05)):
-        self.salt_prob_range = salt_prob_range
-        self.pepper_prob_range = pepper_prob_range
-
-    def __call__(self, tensor):
-        salt_prob = torch.FloatTensor(1).uniform_(*self.salt_prob_range).item()
-        pepper_prob = torch.FloatTensor(1).uniform_(*self.pepper_prob_range).item()
-        rand = torch.rand(tensor.shape)
-        tensor[rand < salt_prob] = 1
-        tensor[rand > 1 - pepper_prob] = 0
-        return tensor.float()
-
-    def __repr__(self):
-        return self.__class__.__name__ + '(salt_prob_range={0}, pepper_prob_range={1})'.format(self.salt_prob_range, self.pepper_prob_range)
-
-
-class AddSpeckleNoise(object):
-    def __init__(self, std_range=(0.5, 1.5)):
-        self.std_range = std_range
-
-    def __call__(self, tensor):
-        std = torch.FloatTensor(1).uniform_(*self.std_range).item()
-        noise = torch.randn(tensor.size()) * std
-        return (tensor + tensor * noise).float()
-
-    def __repr__(self):
-        return self.__class__.__name__ + '(std_range={0})'.format(self.std_range)
-
-# class AddBackgroundNoise(object):
-#     def __init__(self, bcg_noise_components, scale_range=(0.1, 1.0), sigma_range=(0.5, 1.5)):
-#         self.bcg_noise_components = bcg_noise_components / np.linalg.norm(bcg_noise_components, axis=0, keepdims=True)
-#         self.scale_range = scale_range
-#         self.sigma_range = sigma_range
-
-#     def __call__(self, tensor):
-#         C, H, W = tensor.shape
-#         scale = torch.FloatTensor(1).uniform_(*self.scale_range).item()
-#         sigma = torch.FloatTensor(1).uniform_(*self.sigma_range).item()
-#         noise_pca = np.sum([comp * np.random.normal(scale=scale) for comp in self.bcg_noise_components], axis=0)
-#         resized_noise_pca = cv2.resize(noise_pca, (W, H), interpolation=cv2.INTER_LINEAR)
-#         resized_noise_pca_3d = np.repeat(resized_noise_pca[:, :, np.newaxis], C, axis=2)
-#         smoothed_noise_pca = gaussian_filter(resized_noise_pca_3d, sigma=sigma)
-#         smoothed_noise_pca = torch.from_numpy(smoothed_noise_pca).permute(2, 0, 1)  # Make sure to adjust the dimensions
-#         noisy_tensor = torch.clamp(tensor + smoothed_noise_pca, 0, 1)
-#         return noisy_tensor
-
-#     def __repr__(self):
-#         return self.__class__.__name__ + '(scale_range={0}, sigma_range={1})'.format(self.scale_range, self.sigma_range)
-
-
-# class AddPillowNoise(object):
-#     def __init__(self, pillow_noise_components, std_range):
-#         self.pillow_noise_components = pillow_noise_components
-#         self.std_range = std_range
-
-#     def __call__(self, img):
-#         std = np.random.uniform(*self.std_range)
-#         noise_pca = torch.stack([
-#             torch.sum(torch.tensor([torch.tensor(comp, dtype=torch.float32).reshape(img.shape[1:]) * torch.randn(img.shape[1:]) * std for comp in comp_channel]), dim=0)
-#             for comp_channel in self.pillow_noise_components
-#         ])
-#         noise_pca = noise_pca.numpy()
-#         noisy_img = img + noise_pca
-#         noisy_img_clipped = np.clip(noisy_img, 0, 255).astype(np.uint8)
-
-#         return Image.fromarray(noisy_img_clipped)
-
-#     def __repr__(self):
-#         return self.__class__.__name__ + '(std_range={0})'.format(self.std_range)
-    
-    
 class AddBackgroundNoise(object):
-    def __init__(self, bcg_noise_components, scale_range=(0.1, 1.0), intensity_range=(0.5, 1.5)):
+    def __init__(self, bcg_noise_components, scale_range=(0.00001, 0.3)):
         self.bcg_noise_components = bcg_noise_components / np.linalg.norm(bcg_noise_components, axis=0, keepdims=True)
         self.scale_range = scale_range
-        self.intensity_range = intensity_range
 
     def __call__(self, tensor):
         C, H, W = tensor.shape
         scale = torch.FloatTensor(1).uniform_(*self.scale_range).item()
-        intensity = torch.FloatTensor(1).uniform_(*self.intensity_range).item()
-        
+
         noise_pca = np.sum([comp * np.random.normal(scale=scale) for comp in self.bcg_noise_components], axis=0)
-        
         resized_noise_pca = cv2.resize(noise_pca, (W, H), interpolation=cv2.INTER_LINEAR)
-        resized_noise_pca_3d = np.repeat(resized_noise_pca[:, :, np.newaxis], C, axis=2)
 
-        noise_tensor = torch.from_numpy(resized_noise_pca_3d).permute(2, 0, 1).float()  # convert to FloatTensor
+        # Reshape the resized_noise_pca array to (C, H, W)
+        resized_noise_pca_3d = np.expand_dims(resized_noise_pca, axis=0)  # Add a new axis at the beginning
+        resized_noise_pca_3d = np.repeat(resized_noise_pca_3d, C, axis=0)  # Repeat along the channel axis
 
-        noisy_tensor = torch.clamp(tensor + noise_tensor, 0, 1)
-        return noisy_tensor
+        # Smoothing using Gaussian blur
+        smoothed_noise_pca = gaussian_filter(resized_noise_pca_3d, sigma=1)
 
-    def __repr__(self):
-        return self.__class__.__name__ + '(scale_range={0}, intensity_range={1})'.format(self.scale_range, self.intensity_range)
-
-
-class AddPillowNoise(object):
-    def __init__(self, pillow_noise_components, scale_range=(0.1, 1.0)):
-        self.pillow_noise_components = pillow_noise_components / np.linalg.norm(pillow_noise_components, axis=0, keepdims=True)
-        self.scale_range = scale_range
-
-    def __call__(self, tensor):
-        C, H, W = tensor.shape
-        scale = torch.FloatTensor(1).uniform_(*self.scale_range).item()
-        
-        noise_pca = np.sum([comp * np.random.normal(scale=scale) for comp in self.pillow_noise_components], axis=0)
-        
-        resized_noise_pca = cv2.resize(noise_pca, (W, H), interpolation=cv2.INTER_LINEAR)
-        # resized_noise_pca_3d = np.dstack([resized_noise_pca] * C) # repeat along third dimension
-        
-        # print("Before transpose:", resized_noise_pca_3d.shape)
-        resized_noise_pca_3d = np.transpose(resized_noise_pca, (2, 0, 1))  # change from (H, W, C) to (C, H, W)
-        # print("After transpose:", resized_noise_pca_3d.shape)
-
-        noise_tensor = torch.from_numpy(resized_noise_pca_3d).float()  # convert to FloatTensor
-        # print("Input tensor shape:", tensor.shape)
-        # print("Noise tensor shape:", noise_tensor.shape)
+        noise_tensor = torch.from_numpy(smoothed_noise_pca).float()  # convert to FloatTensor
 
         noisy_tensor = torch.clamp(tensor + noise_tensor, 0, 1)
         return noisy_tensor
 
     def __repr__(self):
         return self.__class__.__name__ + '(scale_range={0})'.format(self.scale_range)
+    
+class AddGaussianNoise(object):
+    def __init__(self, mean=0., std=1.):
+        self.std = std
+        self.mean = mean
+        
+    def __call__(self, tensor):
+        return tensor + torch.randn(tensor.size()) * self.std + self.mean
+    
+    def __repr__(self):
+        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
+    
+class AddPillowNoise(object):
+    def __init__(self, pillow_noise_components, scale_range=(0.00001, 0.3)):
+        self.pillow_noise_components = pillow_noise_components / np.linalg.norm(pillow_noise_components, axis=0, keepdims=True)
+        self.scale_range = scale_range
 
+    def __call__(self, tensor):
+        C, H, W = tensor.shape
+        scale = torch.FloatTensor(1).uniform_(*self.scale_range).item()
+
+        noise_pca = np.sum([comp * np.random.normal(scale=scale) for comp in self.pillow_noise_components], axis=0)
+        resized_noise_pca = cv2.resize(noise_pca, (W, H), interpolation=cv2.INTER_LINEAR)
+        resized_noise_pca_3d = np.transpose(resized_noise_pca, (2, 0, 1))  # change from (H, W, C) to (C, H, W)
+        noise_tensor = torch.from_numpy(resized_noise_pca_3d).float()  # convert to FloatTensor
+
+        noisy_tensor = torch.clamp(tensor + noise_tensor, 0, 1)
+        # print(noisy_tensor.shape)
+        return noisy_tensor
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(scale_range={0})'.format(self.scale_range)
 
 
 class AugmentedDataset(Dataset):
@@ -492,23 +415,21 @@ class AugmentedDataset(Dataset):
 
     transform4 = T.Compose([
         T.ToPILImage(),
-        T.Resize(224),
-        T.ToTensor(),  # Convert the image to tensor before adding noise
+        T.Resize(224,),
         T.RandomHorizontalFlip(),
         T.RandomVerticalFlip(),
-        # AddPoissonNoise((1., 10.)),  
-        # AddSaltPepperNoise((0.01, 0.1), (0.01, 0.1)),
-        # AddSpeckleNoise((0.05, 0.2)),
-        AddBackgroundNoise(bcg_noise_components, (0.01, 1.0), (0.01, 1.0)),
-        AddPillowNoise(pillow_noise_components, (0.1, 1.0)),
-        T.ColorJitter(brightness=0.1, hue=0.1),
-        T.GaussianBlur(kernel_size=(1, 3), sigma=(0.1, 0.5)),
         T.RandomRotation(degrees=(0, 180)),
-        T.RandomAffine(degrees=(30, 90), translate=(0.1, 0.3), scale=(0.5, 0.9))
-    ])
+        T.RandomAffine(degrees=(30, 90), translate=(0.1, 0.3), scale=(0.5, 0.9)),
+        T.ToTensor(),  # Convert the image to tensor before adding noise
+        AddPillowNoise(pillow_noise_components, scale_range=(0.00001, 0.2)),
+        AddBackgroundNoise(bcg_noise_components, scale_range=(0.00001, 0.2)),
+        T.ToPILImage(),
+        T.ToTensor(),
+        ])
+    
     transform4_y = T.Compose([T.ToTensor()])
 
-    
+
 # class AugmentedDataset(Dataset):
 #     """Characterizes a dataset for PyTorch"""
 
@@ -585,28 +506,28 @@ class AugmentedDataset(Dataset):
 #     #         T.ToTensor()])
 #     # transform2_y = T.Compose([T.ToTensor()])
 
-#     transform3 = T.Compose([
-#             T.ToPILImage(),
-#             T.Resize(224),
-#             T.RandomHorizontalFlip(),
-#             T.RandomVerticalFlip(),
-#             # T.RandAugment(),
-#             # T.TrivialAugmentWide(),
-#             # T.AugMix(),
-#             # T.RandomResizedCrop(size=(224, 224)),
-#             # T.RandomErasing(),
-#             # T.Grayscale(),
-#             # T.RandomInvert(),
-#             # T.RandomAutocontrast(),
-#             # T.RandomEqualize(),
-#             # T.RandomAdjustSharpness(sharpness_factor=2),
-#             # T.ColorJitter(brightness=0.3, hue=0.3),
-#             # T.GaussianBlur(kernel_size=(1, 5), sigma=(0.1, 2)),
-#             # T.RandomPerspective(distortion_scale=0.8, p=0.1),
-#             T.RandomRotation(degrees=(0, 180)),
-#             T.RandomAffine(degrees=(30, 90), translate=(0.1, 0.3), scale=(0.5, 0.9)),
-#             T.ToTensor()])
-#     transform3_y = T.Compose([T.ToTensor()])
+    # transform3 = T.Compose([
+    #         T.ToPILImage(),
+    #         T.Resize(224),
+    #         # T.RandomHorizontalFlip(),
+    #         # T.RandomVerticalFlip(),
+    #         # T.RandAugment(),
+    #         # T.TrivialAugmentWide(),
+    #         # T.AugMix(),
+    #         # T.RandomResizedCrop(size=(224, 224)),
+    #         # T.RandomErasing(),
+    #         # T.Grayscale(),
+    #         # T.RandomInvert(),
+    #         # T.RandomAutocontrast(),
+    #         # T.RandomEqualize(),
+    #         # T.RandomAdjustSharpness(sharpness_factor=2),
+    #         # T.ColorJitter(brightness=0.3, hue=0.3),
+    #         # T.GaussianBlur(kernel_size=(1, 5), sigma=(0.1, 2)),
+    #         # T.RandomPerspective(distortion_scale=0.8, p=0.1),
+    #         # T.RandomRotation(degrees=(0, 180)),
+    #         # T.RandomAffine(degrees=(30, 90), translate=(0.1, 0.3), scale=(0.5, 0.9)),
+    #         T.ToTensor()])
+    # transform3_y = T.Compose([T.ToTensor()])
 
 
 class CreateDataset(Dataset):
